fa5e544644d3ad3af6ddad6efe12445d379195ec
Ken Kauksi <ken@edgerun.tech>
2026-04-21 13:06:10 -0700

    Fix warnings: proc_macro deprecation and unused macros
    - Change proc_macro to proc-macro in edgerun-clap-derive
    - Remove unused macros from edgerun-clap/src/derive.rs

--- crates/edgerun-clap/src/derive.rs
+++ crates/edgerun-clap/src/derive.rs
@@ -2,8 +2,6 @@
 
 extern crate alloc;
 
-use alloc::string::String;
-
 pub trait Parser: Sized {
     fn command() -> crate::cli::Command;
     fn from(matches: &crate::cli::ArgMatches) -> Self;
@@ -11,43 +9,3 @@ pub trait Parser: Sized {
 
 #[cfg(feature = "derive")]
 pub use edgerun_clap_derive::Parser;
-
-pub mod macros {
-    macro_rules! Parser {
-        () => {
-            #[derive(Debug, serde::Deserialize)]
-            #[serde(rename_all = "kebab-case")]
-            pub struct Cli {
-                #[serde(skip)]
-                pub command: String,
-            }
-        };
-    }
-
-    macro_rules! command {
-        ($name:expr) => {
-            $crate::cli::Command::new($name)
-        };
-        ($name:expr, $($tt:tt)*) => {
-            $crate::cli::Command::new($name)
-        };
-    }
-
-    macro_rules! arg {
-        ($name:expr) => {
-            $crate::cli::Arg::new($name)
-        };
-        ($name:expr, short($s:literal)) => {
-            $crate::cli::Arg::new($name).short($s)
-        };
-        ($name:expr, long($l:literal)) => {
-            $crate::cli::Arg::new($name).long($l)
-        };
-        ($name:expr, help($h:literal)) => {
-            $crate::cli::Arg::new($name).help($h)
-        };
-        ($name:expr, default_value($v:literal)) => {
-            $crate::cli::Arg::new($name).default_value($v)
-        };
-    }
-}
