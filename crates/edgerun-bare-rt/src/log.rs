f3a72fe550968feaf299a43446ead8f67c148044
ken <ken@fw>
2026-04-27 03:38:48 +0700

    bare-rt: simplify logger

--- crates/edgerun-bare-rt/src/log.rs
+++ crates/edgerun-bare-rt/src/log.rs
@@ -1,79 +1,26 @@
-pub struct Level(pub u8);
+pub struct Level(u8);
 
 impl Level {
-    pub const Error: Self = Self(1);
-    pub const Warn: Self = Self(2);
-    pub const Info: Self = Self(3);
-    pub const Debug: Self = Self(4);
-}
-
-pub static LOG_LEVEL: Level = Level::Info;
-
-#[inline]
-pub fn set_level(level: Level) {
-    let _ = level;
-}
-
-pub fn set_logger(_logger: fn(u8, &str)) {
+    pub const ERROR: Self = Self(1);
+    pub const WARN: Self = Self(2);
+    pub const INFO: Self = Self(3);
+    pub const DEBUG: Self = Self(4);
 }
 
 #[inline]
-pub fn log(level: u8, msg: &str) {
-    if level >= LOG_LEVEL.0 {
-        #[cfg(target_arch = "x86_64")]
-        {
-            puts(msg);
-        }
-    }
-}
-
-#[cfg(target_arch = "x86_64")]
-fn puts(s: &str) {
-    let mut pos = 0usize;
-    for byte in s.bytes() {
-        if byte == b'\n' {
-            pos = (pos / 160 + 1) * 160;
-            continue;
-        }
-        if byte >= 0x20 && pos < 4000 {
-            let addr = 0xB8000 as *mut u8;
-            unsafe {
-                addr.add(pos).write_volatile(byte);
-                addr.add(pos + 1).write_volatile(0x07);
+pub fn log(_level: u8, _msg: &str) {
+    #[cfg(target_arch = "x86_64")]
+    {
+        let mut pos = 0usize;
+        for byte in _msg.bytes() {
+            if byte >= 0x20 {
+                let addr = 0xB8000 as *mut u8;
+                unsafe {
+                    addr.add(pos).write_volatile(byte);
+                    addr.add(pos + 1).write_volatile(0x07);
+                }
+                pos += 2;
             }
-            pos += 2;
         }
     }
-}
-
-#[macro_export]
-macro_rules! error {
-    ($($arg:tt)*) => {
-        #[cfg(target_arch = "x86_64")]
-        crate::log::log(1, &alloc::format!($($arg)*));
-    };
-}
-
-#[macro_export]
-macro_rules! warn {
-    ($($arg:tt)*) => {
-        #[cfg(target_arch = "x86_64")]
-        crate::log::log(2, &alloc::format!($($arg)*));
-    };
-}
-
-#[macro_export]
-macro_rules! info {
-    ($($arg:tt)*) => {
-        #[cfg(target_arch = "x86_64")]
-        crate::log::log(3, &alloc::format!($($arg)*));
-    };
-}
-
-#[macro_export]
-macro_rules! debug {
-    ($($arg:tt)*) => {
-        #[cfg(target_arch = "x86_64")]
-        crate::log::log(4, &alloc::format!($($arg)*));
-    };
 }
