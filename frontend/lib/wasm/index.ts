5b270fc0ee59ac7c3ad63b40de4ecb6ffff22854
Ken Kauksi <kensservices@gmail.com>
2026-05-04 23:46:54 +0700

    Remove legacy browser wasm runtime implementation

--- frontend/lib/wasm/index.ts
+++
@@ -1,8 +0,0 @@
-export { EventQueue, EventType, NetworkSubtype, DiskSubtype, encodeNetworkEvent, encodeDiskEvent, encodeTimerEvent } from "./event-queue"
-export { decodeUINode, type UINode } from "./protobuf-ui"
-export { UIRenderer } from "./dom-renderer"
-export { WasmAdapter, type WasmHostCallbacks } from "./wasm-adapter"
-export { installedWasmListStore, getWasmFromCache, installWasm, removeWasm, fetchWasmPackage, fetchWasmFromUrl, type CachedWasm } from "@/stores/wasm-store"
-import { edgerun as streamTypes } from "@/gen/edgerun/v0/stream"
-export type AppPackage = streamTypes.v0.stream.AppPackage
-export { fetchWasmModule, formatWasmSize, WasmSource, type WasmFetchOptions, type WasmFetchResult } from "./wasm-fetcher"
