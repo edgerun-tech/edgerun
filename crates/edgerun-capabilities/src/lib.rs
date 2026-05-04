9e1546a4e6f7c9c4d92234dc37eb8f704512b486
ken <ken@fw>
2026-05-04 10:52:26 +0700

    Use runtime capability protocol types

--- crates/edgerun-capabilities/src/lib.rs
+++ crates/edgerun-capabilities/src/lib.rs
@@ -5,7 +5,7 @@ extern crate alloc;
 use alloc::string::String;
 use alloc::vec::Vec;
 
-pub use edgerun_core::protocol::{
+pub use edgerun_core::protocol::capability::{
     CapabilityAccessClass, CapabilityConstraint, CapabilityConstraintKind, CapabilityDescriptor,
     CapabilityEventKind, CapabilityGrant, CapabilityInvocation, CapabilityModality,
     CapabilityOperation, CapabilityRequest, CapabilityResult, CapabilityRevocation, CapabilityRole,
