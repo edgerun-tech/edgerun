a54525a456d91cd1b57bd877c4625d8dd43654f7
Ken Kauksi <kensservices@gmail.com>
2026-05-05 12:32:20 +0700

    Add protocol signer traits and helpers

---
+++ crates/edgerun-sign/src/lib.rs
@@ -0,0 +1,155 @@
+#![no_std]
+
+extern crate alloc;
+
+use alloc::vec::Vec;
+
+use edgerun_core::protocol::{
+    AssuranceClaim, CommandEnvelope, DelegationRecord, EventEnvelope, IdentityRecord,
+    ProtocolRecord, RevocationRecord, RouteAdvertisement, Signature,
+};
+use edgerun_verify::{protocol_record_hash, protocol_signable_bytes, ProtocolFamily, ProtocolVerifyError};
+
+pub use edgerun_verify::ProtocolFamily as SignableProtocolFamily;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ProtocolSignError {
+    UnsupportedFamily,
+    SignerFailed,
+}
+
+impl From<ProtocolVerifyError> for ProtocolSignError {
+    fn from(value: ProtocolVerifyError) -> Self {
+        match value {
+            ProtocolVerifyError::UnsupportedFamily => Self::UnsupportedFamily,
+            _ => Self::SignerFailed,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ProtocolSigningInput {
+    pub family: ProtocolFamily,
+    pub canonical_bytes: Vec<u8>,
+    pub record_hash: Vec<u8>,
+    pub signature_input: Vec<u8>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ProtocolSigningOutput {
+    pub family: ProtocolFamily,
+    pub signature: Signature,
+    pub record_hash: Vec<u8>,
+}
+
+pub trait ProtocolSigner {
+    fn signature_algorithm(&self) -> i32;
+
+    fn sign_signature_input(
+        &self,
+        family: ProtocolFamily,
+        signature_input: &[u8],
+    ) -> Result<Vec<u8>, ProtocolSignError>;
+
+    fn sign_protocol_record(
+        &self,
+        record: &ProtocolRecord,
+        family: ProtocolFamily,
+    ) -> Result<ProtocolSigningOutput, ProtocolSignError> {
+        let input = protocol_signing_input(record, family)?;
+        let value = self.sign_signature_input(family, &input.signature_input)?;
+        Ok(ProtocolSigningOutput {
+            family,
+            signature: Signature {
+                algorithm: self.signature_algorithm(),
+                value,
+            },
+            record_hash: input.record_hash,
+        })
+    }
+}
+
+pub fn protocol_signing_input(
+    record: &ProtocolRecord,
+    family: ProtocolFamily,
+) -> Result<ProtocolSigningInput, ProtocolSignError> {
+    let canonical_bytes = protocol_signable_bytes(record, family)?;
+    let record_hash = protocol_record_hash(record, family)?;
+    let signature_input = edgerun_core::crypto::signature_input(family.sig_domain(), &record_hash);
+    Ok(ProtocolSigningInput {
+        family,
+        canonical_bytes,
+        record_hash,
+        signature_input,
+    })
+}
+
+pub fn sign_event_envelope<S: ProtocolSigner>(
+    signer: &S,
+    event: &EventEnvelope,
+) -> Result<ProtocolSigningOutput, ProtocolSignError> {
+    signer.sign_protocol_record(
+        &ProtocolRecord::EventEnvelope(event.clone()),
+        ProtocolFamily::EventEnvelope,
+    )
+}
+
+pub fn sign_command_envelope<S: ProtocolSigner>(
+    signer: &S,
+    command: &CommandEnvelope,
+) -> Result<ProtocolSigningOutput, ProtocolSignError> {
+    signer.sign_protocol_record(
+        &ProtocolRecord::CommandEnvelope(command.clone()),
+        ProtocolFamily::CommandEnvelope,
+    )
+}
+
+pub fn sign_delegation_record<S: ProtocolSigner>(
+    signer: &S,
+    delegation: &DelegationRecord,
+) -> Result<ProtocolSigningOutput, ProtocolSignError> {
+    signer.sign_protocol_record(
+        &ProtocolRecord::DelegationRecord(delegation.clone()),
+        ProtocolFamily::DelegationRecord,
+    )
+}
+
+pub fn sign_revocation_record<S: ProtocolSigner>(
+    signer: &S,
+    revocation: &RevocationRecord,
+) -> Result<ProtocolSigningOutput, ProtocolSignError> {
+    signer.sign_protocol_record(
+        &ProtocolRecord::RevocationRecord(revocation.clone()),
+        ProtocolFamily::RevocationRecord,
+    )
+}
+
+pub fn sign_identity_record<S: ProtocolSigner>(
+    signer: &S,
+    identity: &IdentityRecord,
+) -> Result<ProtocolSigningOutput, ProtocolSignError> {
+    signer.sign_protocol_record(
+        &ProtocolRecord::IdentityRecord(identity.clone()),
+        ProtocolFamily::IdentityRecord,
+    )
+}
+
+pub fn sign_assurance_claim<S: ProtocolSigner>(
+    signer: &S,
+    claim: &AssuranceClaim,
+) -> Result<ProtocolSigningOutput, ProtocolSignError> {
+    signer.sign_protocol_record(
+        &ProtocolRecord::AssuranceClaim(claim.clone()),
+        ProtocolFamily::AssuranceClaim,
+    )
+}
+
+pub fn sign_route_advertisement<S: ProtocolSigner>(
+    signer: &S,
+    route: &RouteAdvertisement,
+) -> Result<ProtocolSigningOutput, ProtocolSignError> {
+    signer.sign_protocol_record(
+        &ProtocolRecord::RouteAdvertisement(route.clone()),
+        ProtocolFamily::RouteAdvertisement,
+    )
+}
