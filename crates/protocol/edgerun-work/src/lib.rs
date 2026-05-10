8486052eb0282ebaed3080f8c51709d09d402b11
Ken Kauksi <kensservices@gmail.com>
2026-05-11 06:56:41 +0700

    Wire work request auth module

--- crates/protocol/edgerun-work/src/lib.rs
+++ crates/protocol/edgerun-work/src/lib.rs
@@ -1,15 +1,15 @@
-#![cfg_attr(not(feature = "std"), no_std)]
-
 extern crate alloc;
 
 pub mod codec;
 pub mod protocol;
+pub mod request_auth;
 
 #[cfg(feature = "std")]
 pub mod std_runtime;
 
 pub use codec::*;
 pub use protocol::*;
+pub use request_auth::*;
 
 #[cfg(feature = "std")]
 pub use std_runtime::*;
