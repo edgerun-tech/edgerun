9776951664e67b7c85af2c08c7924fb1608aab8a
Ken Kauksi <kensservices@gmail.com>
2026-05-10 21:31:55 +0700

    Extend UEFI Bluetooth header for Realtek firmware loading

--- firmware/edgerun-efi-bt/EdgerunEfiBt.h
+++ firmware/edgerun-efi-bt/EdgerunEfiBt.h
@@ -3,6 +3,9 @@
 
 #include <Uefi.h>
 #include <Protocol/UsbIo.h>
+#include <Protocol/LoadedImage.h>
+#include <Protocol/SimpleFileSystem.h>
+#include <Guid/FileInfo.h>
 #include <Library/UefiLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/BaseMemoryLib.h>
@@ -43,6 +46,21 @@
 
 #define RTL_COMPANY_ID              0x005D
 #define RTL_ROM_LMP_8922A           0x8922
+#define RTL8922A_HCI_REVISION       0x000A
+#define RTL8922A_HCI_VERSION        0x0C
+#define RTL8922A_PROJECT_ID         44
+
+#define RTL_FRAG_LEN                252
+#define RTL_EPATCH_SIGNATURE        "Realtech"
+#define RTL_EPATCH_SIGNATURE_V2     "RTBTCore"
+#define RTL_EXTENSION_SIG_0         0x51
+#define RTL_EXTENSION_SIG_1         0x04
+#define RTL_EXTENSION_SIG_2         0xFD
+#define RTL_EXTENSION_SIG_3         0x77
+
+#define RTL_PATCH_SNIPPETS          0x01
+#define RTL_PATCH_DUMMY_HEADER      0x02
+#define RTL_PATCH_SECURITY_HEADER   0x03
 
 #define EDGERUN_USB_TIMEOUT_MS      1000
 #define EDGERUN_HCI_EVENT_TIMEOUT_MS 50
@@ -66,9 +84,40 @@ typedef struct {
   UINT16 LmpPalSubversion;
 } EDGERUN_HCI_LOCAL_VERSION;
 
+typedef struct {
+  UINT8 *FwData;
+  UINTN FwLen;
+  UINT8 *CfgData;
+  UINTN CfgLen;
+} EDGERUN_RTL_FIRMWARE_FILES;
+
+typedef struct {
+  UINT8 *Data;
+  UINTN Len;
+} EDGERUN_RTL_PATCH_IMAGE;
+
 EFI_STATUS
 EdgerunFindRtl8922UsbBluetooth(
   OUT EDGERUN_BT_USB *Device
+  );
+
+EFI_STATUS
+EdgerunLoadFileFromBootVolume(
+  IN EFI_HANDLE ImageHandle,
+  IN CONST CHAR16 *Path,
+  OUT UINT8 **Data,
+  OUT UINTN *DataLen
+  );
+
+VOID
+EdgerunFreeFirmwareFiles(
+  IN OUT EDGERUN_RTL_FIRMWARE_FILES *Files
+  );
+
+EFI_STATUS
+EdgerunLoadRtl8922aFirmwareFiles(
+  IN EFI_HANDLE ImageHandle,
+  OUT EDGERUN_RTL_FIRMWARE_FILES *Files
   );
 
 EFI_STATUS
@@ -100,7 +149,28 @@ EdgerunHciReadBdAddr(
 
 EFI_STATUS
 EdgerunRealtekInitRtl8922A(
-  IN OUT EDGERUN_BT_USB *Device
+  IN OUT EDGERUN_BT_USB *Device,
+  IN EFI_HANDLE ImageHandle
+  );
+
+EFI_STATUS
+EdgerunParseRtl8922aFirmware(
+  IN CONST EDGERUN_RTL_FIRMWARE_FILES *Files,
+  IN CONST EDGERUN_HCI_LOCAL_VERSION *Version,
+  IN UINT8 RomVersion,
+  OUT EDGERUN_RTL_PATCH_IMAGE *Patch
+  );
+
+EFI_STATUS
+EdgerunDownloadRtlFirmware(
+  IN OUT EDGERUN_BT_USB *Device,
+  IN CONST UINT8 *Data,
+  IN UINTN DataLen
+  );
+
+VOID
+EdgerunFreePatchImage(
+  IN OUT EDGERUN_RTL_PATCH_IMAGE *Patch
   );
 
 EFI_STATUS
