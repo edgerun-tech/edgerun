b517e64d5beb43fe59d9df9283c6b7dff25e77bd
Ken Kauksi <kensservices@gmail.com>
2026-05-11 07:17:49 +0700

    Export memory channel engine

--- crates/protocol/edgerun-work/src/lib.rs
+++ crates/protocol/edgerun-work/src/lib.rs
@@ -2,6 +2,7 @@ extern crate alloc;
 
 pub mod channel;
 pub mod codec;
+pub mod memory_channel;
 pub mod protocol;
 pub mod request_auth;
 pub mod roles;
@@ -11,6 +12,7 @@ pub mod std_runtime;
 
 pub use channel::*;
 pub use codec::*;
+pub use memory_channel::*;
 pub use protocol::*;
 pub use request_auth::*;
 pub use roles::*;
