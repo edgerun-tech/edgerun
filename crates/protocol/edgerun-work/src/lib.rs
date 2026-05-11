d235167a9580281152fa5e9b4ee6bcbf3e53da5e
Ken Kauksi <kensservices@gmail.com>
2026-05-11 07:12:39 +0700

    Export channel route protocol

--- crates/protocol/edgerun-work/src/lib.rs
+++ crates/protocol/edgerun-work/src/lib.rs
@@ -1,15 +1,19 @@
 extern crate alloc;
 
+pub mod channel;
 pub mod codec;
 pub mod protocol;
 pub mod request_auth;
+pub mod roles;
 
 #[cfg(feature = "std")]
 pub mod std_runtime;
 
+pub use channel::*;
 pub use codec::*;
 pub use protocol::*;
 pub use request_auth::*;
+pub use roles::*;
 
 #[cfg(feature = "std")]
 pub use std_runtime::*;
