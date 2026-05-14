a83be8cf03441200e15e7694762a75c679c6befc
ken <ken@fw>
2026-05-14 14:45:44 +0700

    Own Deno ICU data

--- crates/utility/edgerun-deno-core-icudata/src/lib.rs
+++ crates/utility/edgerun-deno-core-icudata/src/lib.rs
@@ -1,1 +1,7 @@
-pub use deno_core_icudata::*;
+#[repr(C, align(16))]
+struct IcuData<T: ?Sized>(T);
+
+static ICU_DATA_RAW: &IcuData<[u8]> = &IcuData(*include_bytes!("icudtl.dat"));
+
+/// Raw ICU data for V8-compatible code paths.
+pub static ICU_DATA: &[u8] = &ICU_DATA_RAW.0;
